47ac509f98798231414b85ca316be9881473707a
Natália Pereira <78566867+natybastosp@users.noreply.github.com>
2026-04-08 20:07:53 -0300

    feat: add GSAP animation to Navbar for enhanced scroll effects

--- mocktail/src/components/Navbar.jsx
+++ mocktail/src/components/Navbar.jsx
@@ -1,6 +1,28 @@
+import { useGSAP } from "@gsap/react";
+import gsap from "gsap";
 import { navLinks } from "../../constants";
 
 const Navbar = () => {
+  useGSAP(() => {
+    const navTween = gsap.timeline({
+      scrollTrigger: {
+        trigger: "nav",
+        start: "bottom top",
+      },
+    });
+
+    navTween.fromTo(
+      "nav",
+      { backgroundColor: "transparent" },
+      {
+        backgroundColor: "#00000050",
+        backgroundFilter: "blur(10px)",
+        duration: 1,
+        ease: "power1.inOut",
+      },
+    );
+  });
+
   return (
     <nav>
       <div>
